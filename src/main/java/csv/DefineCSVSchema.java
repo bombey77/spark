038e81fb9c4e110a4e26f97ece1227f58ac0f530
Roman Motovilov <bombey131@gmail.com>
2020-11-10 01:57:18 +0200

    define schema for CSV file example

--- src/main/java/csv/DefineCSVSchema.java
+++ src/main/java/csv/DefineCSVSchema.java
@@ -5,8 +5,11 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
-public class ReadFromCSV {
+public class DefineCSVSchema {
 
   public static void main(String[] args) {
     Logger.getLogger("org").setLevel(Level.ERROR);
@@ -18,6 +21,17 @@ public class ReadFromCSV {
             .master("local[1]")
             .getOrCreate();
 
+    StructType schema = DataTypes.createStructType(new StructField[]{
+            DataTypes.createStructField("id", DataTypes.IntegerType, false),
+            DataTypes.createStructField("brand", DataTypes.StringType, false),
+            DataTypes.createStructField("model", DataTypes.StringType, false),
+            DataTypes.createStructField("engine", DataTypes.StringType, false),
+            DataTypes.createStructField("doors", DataTypes.IntegerType, false),
+            DataTypes.createStructField("color", DataTypes.StringType, false),
+            DataTypes.createStructField("date", DataTypes.DateType, false),
+            DataTypes.createStructField("description", DataTypes.StringType, false)
+    });
+
     Dataset<Row> df = spark
             .read()
             .format("csv")
@@ -26,21 +40,21 @@ public class ReadFromCSV {
             .option("encoding", "utf-8")
             .option("sep", ";")//устанавливаю разделитель ';'
             .option("quote", "^")//устанавливаю, что кавычки - это знак каретки и они будут игнорироваться при выводе (до ^6 straight^, после 6 straight)
-            .option("dateFormat", "M/d/y")
-            .option("inferSchema", true)//колонки будут отображаться не как стринг, а отображать тип который они содержат (пример: integer)
+            .option("dateFormat", "MM/dd/yy")
+            .schema(schema)
             .load("/media/roman/4C5A0611FB135B42/Project/spark/import/cars.csv");
 //    df.show(2, 10); выведет только первые 2 строчки и обрежет данные в колонках, где длина свыше 10 символов
     df.show();
     df.printSchema();
   }
 
-//  +---+--------+------+----------+-----+-----+--------+--------------------+
-//  | id|   brand| model|    engine|doors|color|    date|         description|
-//  +---+--------+------+----------+-----+-----+--------+--------------------+
-//  |  1|Mercedes|  W124|6 straight|    5|black|12/31/20|   the best car ever|
-//  |  2|     BMW|    M5|      W 12|    3|  red|10/28/08|I would like to h...|
-//  |  3|   Volvo|Tigger|4 straight|    5|white|02/02/12| do not buy this car|
-//  +---+--------+------+----------+-----+-----+--------+--------------------+
+//  +---+--------+------+----------+-----+-----+----------+--------------------+
+//  | id|   brand| model|    engine|doors|color|      date|         description|
+//  +---+--------+------+----------+-----+-----+----------+--------------------+
+//  |  1|Mercedes|  W124|6 straight|    5|black|2020-12-31|   the best car ever|
+//  |  2|     BMW|    M5|      W 12|    3|  red|2008-10-28|I would like to h...|
+//  |  3|   Volvo|Tigger|4 straight|    5|white|2012-02-02| do not buy this car|
+//  +---+--------+------+----------+-----+-----+----------+--------------------+
 //
 //  root
 // |-- id: integer (nullable = true)
@@ -49,6 +63,6 @@ public class ReadFromCSV {
 // |-- engine: string (nullable = true)
 // |-- doors: integer (nullable = true)
 // |-- color: string (nullable = true)
-// |-- date: string (nullable = true)
+// |-- date: date (nullable = true)
 // |-- description: string (nullable = true)
 }
