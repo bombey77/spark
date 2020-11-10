05743a90fcbfcc2524b9d2e2c927353d5a965fac
Roman Motovilov <bombey131@gmail.com>
2020-11-11 00:13:05 +0200

    added dataset reduce example

--- src/main/java/transformation/ReduceTransformation.java
+++ src/main/java/transformation/ReduceTransformation.java
@@ -3,16 +3,16 @@ package transformation;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.ReduceFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
-public class MapTransformation {
+public class ReduceTransformation {
 
   public static void main(String[] args) {
     Logger.getLogger("org").setLevel(Level.ERROR);
@@ -21,7 +21,7 @@ public class MapTransformation {
     SparkSession spark = SparkSession
             .builder()
             .master("local")
-            .appName("Transformation (map)")
+            .appName("Transformation (reduce)")
             .getOrCreate();
 
     List<String> list = Arrays.asList("banana", "lemon", "apple", "cherry", "strawberry");
@@ -29,29 +29,20 @@ public class MapTransformation {
 //    Dataset<String> ds = spark.createDataset(list, Encoders.STRING());
     Dataset<String> ds = spark.createDataset(list, Encoders.STRING());
 //  первый способ
-    ds = ds.map((MapFunction<String, String>) fruit -> "name: " + fruit, Encoders.STRING());
+    String reduce = ds.reduce((ReduceFunction<String>) (fruit1, fruit2) -> fruit1 + "-" + fruit2);
 //    второй способ
-//    ds = ds.map(new FruitMapper(), Encoders.STRING());
-    ds.show();
-
-//    +----------------+
-//    |           value|
-//    +----------------+
-//    |    name: banana|
-//    |     name: lemon|
-//    |     name: apple|
-//    |    name: cherry|
-//    |name: strawberry|
-//    +----------------+
+//    String reduce = ds.reduce(new FruitReducer());
+    System.out.println(reduce);
+//    banana-lemon-apple-cherry-strawberry
   }
 
-  private static class FruitMapper implements MapFunction<String, String>, Serializable {
+  private static class FruitReducer implements ReduceFunction<String>, Serializable {
 
-    private static final long serialVersionUID = 6637103396919011693L;
+    private static final long serialVersionUID = -646268219357565322L;
 
     @Override
-    public String call(String value) {
-      return "name: " + value;
+    public String call(String fruit1, String fruit2) {
+      return fruit1 + "-" + fruit2;
     }
   }
 }
